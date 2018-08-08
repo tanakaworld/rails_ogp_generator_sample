6275e485278b1cd92a3b39dad40c8c48368aa9e8
tanakaworld <yutaro.tanaka.world@gmail.com>
2018-08-08 21:57:58 +0900

    Make the avatar to circle

--- app/models/user_ogp_image_generator.rb
+++ app/models/user_ogp_image_generator.rb
@@ -27,6 +27,7 @@ class UserOgpImageGenerator
     # avatar
     avatar_image = Magick::Image.from_blob(open(avatar_path).read).first
     avatar_image = avatar_image.resize(320, 320)
+    avatar_image = make_circle_mask(avatar_image, 320)
     image.composite!(avatar_image, Magick::CenterGravity, 0, -80, Magick::OverCompositeOp)
 
     # name
@@ -42,5 +43,23 @@ class UserOgpImageGenerator
     dist_path = "#{dist_dir}/#{user_id}-#{user_name}.png"
     image.write(dist_path)
     dist_path
+  end
+
+  private
+
+  def make_circle_mask(image, size)
+    circle_image = Magick::Image.new(size, size)
+    draw = Magick::Draw.new
+
+    # ref: https://rmagick.github.io/draw.html#circle
+    draw.circle(size / 2, size / 2, size / 2, 0)
+    draw.draw(circle_image)
+    mask = circle_image.blur_image(0, 1).negate
+    mask.matte = false
+
+    image.matte = true
+    image.composite!(mask, Magick::CenterGravity, Magick::CopyOpacityCompositeOp)
+
+    image
   end
 end
